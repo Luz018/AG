cbea56035b0bb1d113c1c0b088cb6171130614e2
Luz018 <angelyne018@gmail.com>
2026-09-22 05:12:24 +0100

    GO upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.AUDIENCEGOV_CONFIG = {
+    supabaseUrl: 'https://lgqprlewydtpfrmdhotm.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxncXBybGV3eWR0cGZybWRob3RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxMjYzMzcsImV4cCI6MjEwNDcwMjMzN30.HC3IoBATUwL1QDBeVr4EBEa9cHqpfgM0EZYxvMN97lY'
+};
